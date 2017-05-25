a4a75ab565f1c264f533986627b0693a76faf368
Michele Tobias <mmtobias@ucdavis.edu>
2017-05-25 16:26:43 -0700

    Cleaned up merge script

--- mergeGeoJson.R
+++ mergeGeoJson.R
@@ -1,14 +1,9 @@
 # This R script combines all the GeoJSON files in a folder into one file, then writes it back to the folder.
 # Modified from its original source: https://gist.github.com/wildintellect/582bb1096092170070996d21037b82d8
 
-
-# TODO 
-# try reading each list and then combining them 
-
 library(raster)
 library(geojsonio)
 library(rgdal)
-
 
 # probably want to change the pattern to exclude or filter after to drop the all.geojson file
 avas <- list.files(path="./avas", pattern = "*json$", full.names = "TRUE")
@@ -20,24 +15,10 @@ gj <- c(avas, tbd)
 gj <- gj[gj != "./avas.geojson"]
 gj <- gj[gj != "./tbd/avas.geojson"]
 
-#read all the geojson files with readOGR? or geojsonio package
-avavects  <- lapply(avas, FUN=geojson_read, what="sp")
-vects  <- c()
-
-for (i in gj){
-  tbdi <- geojson_read(i, what="sp")
-  #tbdogr<-ogrInfo(tbdi)
-  #tbdlint<-lint(tbdi)
-  vects <- c(vects, tbdi)
-  #print(tbdogr)
-}
-
+#read all the geojson files
 vects <- lapply(gj, geojson_read, what="sp")
-
 
 #combine all the vectors together, bind is from the raster package
 #probably could just rbind geojson lists too, but thats harder to plot
 all <- do.call(bind, vects)
 geojson_write(all, file="avas.geojson", overwrite=TRUE, convert_wgs84 = TRUE)
-
-# tested 3/30/2017 with 2 geojson polygons -> success -- MicheleTobias 
