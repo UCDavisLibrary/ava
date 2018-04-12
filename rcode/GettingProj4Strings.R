a8062b2f0db82d975c0528646af2414676d47d01
Michele Tobias <MicheleTobias@users.noreply.github.com>
2018-04-12 16:57:14 -0700

    add Yadkin Valley

--- rcode/GettingProj4Strings.R
+++ rcode/GettingProj4Strings.R
@@ -3,8 +3,16 @@
 library(raster)
 library(rgdal)
 
-setwd("C:\\Users\\mmtobias\\Documents\\WineData\\Topos\\TrinityLakes")
-topo<-raster("CA_Papoose Creek_293949_1982_24000_geo.tif")
+setwd("C:\\Users\\mmtobias\\Documents\\WineData\\Topos\\Yadkin")
+topo<-raster("NC_Charlotte_164185_1953_250000_geo.tif")
 crs(topo)
 
 #paste the results into the proj.4 string for your topo in the layer properties to set the correct CRS.
+
+#reproject the topo
+
+newcrs<-crs("+init=epsg:4326")
+
+topo4326<-projectRaster(topo, crs=newcrs)
+
+writeRaster(topo4326, 'Charlotte.tif', overwrite=TRUE)
