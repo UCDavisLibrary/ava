818c134ff6c56f156a7bda3cc8bf6191575af1dc
Michele Tobias <MicheleTobias@users.noreply.github.com>
2018-04-12 16:58:34 -0700

    update Proj4 String code

--- rcode/GettingProj4Strings.R
+++ rcode/GettingProj4Strings.R
@@ -4,12 +4,14 @@ library(raster)
 library(rgdal)
 
 setwd("C:\\Users\\mmtobias\\Documents\\WineData\\Topos\\Yadkin")
-topo<-raster("NC_Charlotte_164185_1953_250000_geo.tif")
+topo<-raster("NC_Winston-Salem_164064_1953_250000_geo.tif")
 crs(topo)
 
 #paste the results into the proj.4 string for your topo in the layer properties to set the correct CRS.
 
-#reproject the topo
+
+
+#reproject the topo if needed... probably the step above will work without having to write a new file.
 
 newcrs<-crs("+init=epsg:4326")
 
