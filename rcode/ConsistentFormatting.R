4f30760ee447aa857db618a8be60ab78def3cabd
Michele Tobias <mmtobias@ucdavis.edu>
2020-09-16 14:38:11 -0700

    Create ConsistentFormatting.R

---
+++ rcode/ConsistentFormatting.R
@@ -0,0 +1,21 @@
+#GOAL: fix the inconsistent formatting in the AVA files as needed.
+
+
+#Libraries
+
+
+#Load Data
+
+
+#Inconsistencies to fix:
+#   1. valid_start & valid_end
+#       - replace "" with NA
+#       - replace / with - for dates
+#       - check date format
+#   2. counties, contains, within
+#       - replace ", " with |
+
+
+
+
+
